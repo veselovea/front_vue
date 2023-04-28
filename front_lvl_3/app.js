fc6b0ac49737d1daf8b0fba96cf80d340ce9330b
MegaAndrey <vesolandrey@gmail.com>
2023-04-28 22:29:57 +0800

    lesson 3

--- front_lvl_3/app.js
+++ front_lvl_3/app.js
@@ -1,5 +1,14 @@
 Vue.createApp({
-
+    data() {
+        return {
+            plans: [ 
+            { name: 'Mega brain', price: 500 },
+            { name: 'GIGA brain', price: 50000 },
+            { name: 'Some brain', price: 250 },
+            { name: 'One more BRAIN', price: 10 }
+        ]
+        }
+    }
 })
 .component('click-counter', {
     template: '#click-counter-template',
@@ -7,6 +16,13 @@ Vue.createApp({
         return {
             count: 0
         }
+    }
+})
+.component('plan', {
+    template: '#plan-template',
+    props: {
+        name: { type: String, default: 'Unknown brain', required: true },
+        price: { type: Number, default: 0, required: true }
     }
 })
 .mount('#app')
